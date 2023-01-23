4689c36b4f048c9d7ef96a6725e690c481925a6b
IchinoseJiro <122766611+IchinoseJiro@users.noreply.github.com>
2023-01-23 11:45:47 +0800

    Add files via upload
    Prelims Initial files

---
+++ prelims/node-example.js
@@ -0,0 +1,14 @@
+//Lowell Jireh P. Chantengco
+//Module: 3
+//Week 2 and 3: Basic of NodeJS
+
+
+//firstprogram
+console.log("welcome to WCSERVER");
+
+// Creating my first function
+function Display(x){
+    console.log(x);
+}
+
+Display(200)
