f5f0ca5872823e8ecdecd908a0169a39aaabf828
IchinoseJiro <122766611+IchinoseJiro@users.noreply.github.com>
2023-01-30 13:01:27 +0800

    1 file added, 1 deleted

--- prelims/Lab-Activity-3/sarbar/Lab-Act-3-NodeJS-Server.js
+++ prelims/Lab-Activity-3/sarbar/Lab-Act-3-NodeJS-Server.js
@@ -4,7 +4,7 @@ var server = http.createServer(function (req, res) {
   if (req.url == '/') {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(
-      '<html><body><h3>Welcome to my Node.js Application</h3><br><br><p>Welcome John Smith. This is an activity about basics of Node.js</p></body></html>'
+      '<html><body><h3>Welcome to my Node.js Application</h3><br><br><p>Welcome Jireh Chantengco. This is an activity about basics of Node.js</p></body></html>'
     );
     res.end();
   } else if (req.url == '/about') {
