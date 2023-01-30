44fa7198c3e4770f965e68867e3a71d13a74bf8c
IchinoseJiro <122766611+IchinoseJiro@users.noreply.github.com>
2023-01-30 12:50:21 +0800

    1 file modified

--- prelims/Lab-Activity-3/sarbar/server.js
+++ prelims/Lab-Activity-3/sarbar/server.js
@@ -1,29 +1,29 @@
 var http = require('http');
 
 var server = http.createServer(function (req, res) {
-if (req.url == '/') {
-res.writeHead(200, { 'Content-Type': 'text/html' });
-res.write(
-'<html><body><h3>Welcome to my Node.js Application</h3><br><br><p>Welcome John Smith. This is an activity about basics of Node.js</p></body></html>'
-);
-res.end();
-} else if (req.url == '/about') {
-res.writeHead(200, { 'Content-Type': 'text/html' });
-res.write(
-'<html><body><h3>This is the About Page</h3><br><br><p>Hello John Smith. This activity will teach on how to deal with a simple server and local modules in Node.js</p></body></html>'
-);
-res.end();
-} else if (req.url == '/contact') {
-res.writeHead(200, { 'Content-Type': 'text/html' });
-res.write(
-'<html><body><h3>This is the Contact Page</h3><br><br><p>John Smith, if you want additional details about this activity go to this site: https://www.tutorialsteacger.com/nodejs/nodejs-tutorials</p></body></html>'
-);
-res.end();
-} else if (req.url == '/about') {
-res.writeHead(200, { 'Content-Type': 'text/html' });
-res.write('<html><body><h3>This is the Gallery Page</h3></body></html>');
-res.end();
-} else res.end('Invalid Request!');
+  if (req.url == '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(
+      '<html><body><h3>Welcome to my Node.js Application</h3><br><br><p>Welcome John Smith. This is an activity about basics of Node.js</p></body></html>'
+    );
+    res.end();
+  } else if (req.url == '/about') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(
+      '<html><body><h3>This is the About Page</h3><br><br><p>Hello John Smith. This activity will teach on how to deal with a simple server and local modules in Node.js</p></body></html>'
+    );
+    res.end();
+  } else if (req.url == '/contact') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(
+      '<html><body><h3>This is the Contact Page</h3><br><br><p>John Smith, if you want additional details about this activity go to this site: https://www.tutorialsteacger.com/nodejs/nodejs-tutorials</p></body></html>'
+    );
+    res.end();
+  } else if (req.url == '/gallery') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<html><body><h3>This is the Gallery Page</h3></body></html>');
+    res.end();
+  } else res.end('Invalid Request!');
 });
 
 server.listen(5000);
