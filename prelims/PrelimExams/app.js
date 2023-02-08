e3059cb1ba3c59afaec37911447eb64ead506b8a
IchinoseJiro <122766611+IchinoseJiro@users.noreply.github.com>
2023-02-08 16:00:43 +0800

    1 file modified

--- prelims/PrelimExams/app.js
+++ prelims/PrelimExams/app.js
@@ -1,6 +1,6 @@
 var { greet, congratulations, farewell } = require('./function.js');
 
-var name = 'Allen Serrano';
+var name = 'Jireh Chantengco';
 
 console.log(greet(name));
 console.log(congratulations(name));
